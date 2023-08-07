845364b53e98074b73670b8292b19edf653258a1
bitcamp\bitcamp <janggun21@naver.com>
2023-08-07 12:16:15 +0900

    한국인 : 회원가입/로그인_test_2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
-import LoginPage from './pages/LoginPage';
+// import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import TestPage from './TestPage';
 
 const App = () => {
   return (
+    // <Routes>
+    //   <Route path="/" element={<Home />} />
+    //   <Route path="/login" element={<LoginPage />} />
+    //   <Route path="/register" element={<RegisterPage />} />
+    // </Routes>
+
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/login" element={<TestPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
+
   );
 };
 
