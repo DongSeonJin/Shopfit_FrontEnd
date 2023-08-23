858edaacb45c240e40f073c821418d7d8e1b77fd
lilylemonoh <lilylemonoh@naver.com>
2023-08-23 20:29:50 +0900

    OH: feat: shopping 상품 상세조회 기본 페이지 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-import HomeContent from '../components/common/HomeContent';
-import Community from '../pages/Community';
-import Shopping from '../pages/Shopping';
-import News from '../pages/News';
+import HomeContent from "../components/common/HomeContent";
+import Community from "../pages/Community";
+import Shopping from "../pages/Shopping";
+import News from "../pages/News";
+import ProductDetail from "../components/shop/ProductDetail";
 
 const Home = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomeContent />} />
-        
+
         <Route path="/community" element={<Community />} />
 
         <Route path="/shopping" element={<Shopping />} />
         <Route path="/shopping/:pageNum" element={<Shopping />} />
         <Route path="/shopping/search/:keyword" element={<Shopping />} />
-        <Route path="/shopping/search/:keyword/:pageNum" element={<Shopping />} />
+        <Route
+          path="/shopping/search/:keyword/:pageNum"
+          element={<Shopping />}
+        />
+
+        <Route
+          path="/shopping/products/:productNum"
+          element={<ProductDetail />}
+        />
 
         <Route path="/news/list" element={<News />} />
         <Route path="/news/list/:pageNum" element={<News />} />
