982b66fc64df7031e0891dc24af7ed46e8f9b24a
Dongja <jds79793@gmail.com>
2023-08-24 09:55:40 +0900

    JIN: Refactor: 커뮤니티 메인화면 구조변경

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,11 @@ import { Route, Routes } from 'react-router-dom';
 
 import HomeContent from '../components/common/HomeContent';
 import Community from '../pages/Community';
-import Community1 from '../components/community/PostList1';
+import PostList from '../components/community/PostList';
 import Community2 from '../components/community/PostList2';
 import Community3 from '../components/community/PostList3';
 import Shopping from '../pages/Shopping';
 import News from '../pages/News';
-import Communityhan from './Communityhan';
 
 const Home = () => {
   return (
@@ -18,10 +17,7 @@ const Home = () => {
         
         {/* <Route path="/community/:categoryId" element={<Community />} /> */}
 
-        <Route path="/community" element={<Communityhan />} />
-        <Route path="/post/list/1" element={<Community1 />} />
-        <Route path="/post/list/2" element={<Community2 />} />
-        <Route path="/post/list/3" element={<Community3 />} />
+        <Route path="/post/list/:categoryId" element={<PostList />} />
 
         <Route path="/shopping" element={<Shopping />} />
         <Route path="/shopping/:pageNum" element={<Shopping />} />
