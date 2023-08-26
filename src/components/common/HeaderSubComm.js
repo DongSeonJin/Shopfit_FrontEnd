fcc3c0738e05d71d0e427d020f09efdf39285bbe
Dongja <jds79793@gmail.com>
2023-08-26 16:15:57 +0900

    JIN: Feat: 커뮤니티 인피니티 스크롤 구현

--- src/components/common/HeaderSubComm.js
+++ src/components/common/HeaderSubComm.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom';
 const HeaderSubComm = () => { 
   return (
     <div className={styles.linkContainer}>
-      <Link to="/post/list/1" className={styles.link}>자유게시판</Link>
-      <Link to="/post/list/2" className={styles.link}>오운완</Link>
-      <Link to="/post/list/3" className={styles.link}>식단</Link> 
+      <Link to="/post/list/1/1" className={styles.link}>자유게시판</Link>
+      <Link to="/post/list/2/1" className={styles.link}>오운완</Link>
+      <Link to="/post/list/3/1" className={styles.link}>식단</Link> 
     </div>
   );
 };
--- src/components/community/PostList.js
+++ src/components/community/PostList.js
@@ -1,36 +1,76 @@
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import styles from '../../styles/community/PostList.module.css'
 
 const PostList = () => {
-    const [posts, setPosts] = useState([]);
-    const { categoryId } = useParams(); // 현재 URL에서 categoryId 파라미터 값을 가져옵니다.
+  const [posts, setPosts] = useState([]);
+  const [pageNumb, setPageNumb] = useState(1);
+  const { categoryId } = useParams();
+  const loader = useRef(null);
 
-    useEffect(() => {
-        // 컴포넌트가 마운트되면 HTTP 요청을 보내서 데이터를 가져옴
-        axios.get(`/post/list/${categoryId}`)
-            .then(response => {
-                // 서버에서 받은 데이터를 상태에 저장
-                setPosts(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
-    },  [categoryId]); // 의존성 배열에 categoryId를 추가하여 해당 값이 변경될 때마다 useEffect 내부의 코드가 실행되도록 만들기.
+  useEffect(() => {
+    var options = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 1.0
+    };
 
+    // Intersection Observer를 생성하고 시작합니다.
+    const observer = new IntersectionObserver(handleObserver, options);
+    if (loader.current) {
+      observer.observe(loader.current)
+    }
 
-    return (
-        <div>
-            <h1>Post List</h1>
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
-                ))}
-            </ul>
+  }, []);
+
+  useEffect(() => {
+    // categoryId가 변경될 때마다 pageNumb를 1로 초기화
+    setPageNumb(1);
+  }, [categoryId]);
+
+  useEffect(() => {
+    // 컴포넌트가 마운트되면 HTTP 요청을 보내서 데이터를 가져옵니다.
+    axios.get(`/post/list/${categoryId}/${pageNumb}`)
+      .then(response => {
+        // 서버에서 받은 데이터를 상태에 저장
+        setPosts(prevPosts => [...prevPosts, ...response.data.content]); 
+        setPageNumb(prevPageNumber => prevPageNumber + 1); // 페이지 번호 증가
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+    
+   }, [categoryId]);
+
+   // loader(ref) 가 화면에 나타났다 사라졌다 할 때 호출되는 함수입니다.
+   const handleObserver = (entities) => {
+    const target = entities[0];
+    if (target.isIntersecting) {   
+      setPageNumb((prevPageNumber) => prevPageNumber + 1)
+    }
+  }
+
+   return (
+     <div className={styles['post-list']}>
+       {posts.map((post) => (
+         <div className={styles['post-card']} key={post.id}>
+           <img src={post.imageUrl} alt={post.title} className={styles['post-image']} />
+           <div className={styles['post-content']}>
+             <h2 className={styles['post-title']}>{post.title}</h2>
+             <p className={styles['post-author']}>{`작성자: ${post.nickname}`}</p>
+             {/* 추가적인 내용들... */}
+           </div>
+         </div>
+        ))}
+        <div ref={loader} className={styles['loading-indicator']}>
+          {/* 로딩 인디케이터 (예: 스피너) */}
+          Loading...
         </div>
+      </div>
     );
 };
 
 export default PostList;
 
+      
