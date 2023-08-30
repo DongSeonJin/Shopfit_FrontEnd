545f06d70b19db130be28dcc1f4edaaa7b9e50a1
lilylemonoh <lilylemonoh@naver.com>
2023-08-30 11:10:03 +0900

    OH: feat: shopping - 상품 등록 다중 이미지, 수정, 삭제 기능 구현

--- src/components/shop/ProductUpdate.js
+++ src/components/shop/ProductUpdate.js
@@ -1,16 +1,18 @@
 import axios from "axios";
-import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
 import FileUploadComponent from "./FileUploadComponent";
 import FilesUploadComponent from "./FilesUploadComponent";
+import { Button } from "@mui/material";
 
-const ProductCreate = () => {
+const ProductUpdate = () => {
   const [ProductCategory, setProductCategory] = useState("");
   const [productName, setProductName] = useState("");
   const [thumbnailUrl, setThumbnailUrl] = useState("");
   const [price, setPrice] = useState("");
   const [stockQuantity, setStockQuantity] = useState("");
   const [productImageUrls, setProductImageUrls] = useState([]);
+  const [productImageIds, setProductImageIds] = useState([]);
 
   const productCategories = [
     { id: 1, name: "닭가슴살" },
@@ -19,6 +21,7 @@ const ProductCreate = () => {
   ];
 
   const navigate = useNavigate();
+  const { productNum } = useParams();
 
   const handleProductCategoryChange = (e) => {
     setProductCategory(e.target.value);
@@ -42,17 +45,12 @@ const ProductCreate = () => {
   };
 
   const handleAddImage = (url) => {
-    // 이미지 URL을 입력 받아 리스트에 추가
+    // 이미지 URL을 입력 받아 리스트에 추가 (중복 체크)
     const imageUrl = url;
-    if (imageUrl) {
+
+    if (imageUrl && !productImageUrls.includes(imageUrl)) {
       setProductImageUrls([...productImageUrls, imageUrl]);
     }
-  };
-
-  const handleRemoveImage = (index) => {
-    // 선택한 인덱스의 이미지를 리스트에서 제거
-    const newImageUrls = productImageUrls.filter((_, i) => i !== index);
-    setProductImageUrls(newImageUrls);
   };
 
   // 비동기적으로 폼 제출 처리하기
@@ -62,9 +60,9 @@ const ProductCreate = () => {
     const data = {
       categoryId: ProductCategory,
       productName: productName,
-      thumbnailUrl: thumbnailUrl,
       price: price,
       stockQuantity: stockQuantity,
+      thumbnailUrl: thumbnailUrl,
       productImageUrls: productImageUrls,
     };
 
@@ -72,13 +70,13 @@ const ProductCreate = () => {
       alert("모든 항목을 입력해주세요.");
     } else {
       try {
-        // 백엔드 서버에 POST 요청 보내기
-        await axios.post("/shopping/save", data, {
+        // 백엔드 서버에 PATCH 요청 보내기
+        await axios.patch(`/shopping/update/${productNum}`, data, {
           headers: {
             "Content-Type": "application/json",
           },
         });
-        alert("상품이 등록되었습니다.");
+        alert("수정이 완료되었습니다.");
 
         // 입력 필드 초기화
         setProductName("");
@@ -96,9 +94,68 @@ const ProductCreate = () => {
     }
   };
 
+  // 이펙트 훅을 사용하여 수정 버튼을 눌렀을 때 데이터 로드 및 초기화
+  useEffect(() => {
+    // 기존 데이터 로딩 및 상태 설정 로직
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/shopping/update/${productNum}`);
+        const productData = response.data;
+
+        setProductCategory(productData.categoryId);
+        setProductName(productData.productName);
+        setThumbnailUrl(productData.thumbnailUrl);
+        setPrice(productData.price);
+        setStockQuantity(productData.stockQuantity);
+        setProductImageUrls(productData.productImageUrls);
+        setProductImageIds(productData.productImageIds);
+      } catch (error) {
+        console.error("상품 정보 로딩 실패");
+        // 실패 처리 로직 추가
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleDeleteThumbnail = (thumbnailUrl) => {
+    const thumbnailObjectKey = thumbnailUrl.split("/").pop(); // 마지막 부분을 추출하여 S3 객체 키로 사용
+
+    axios
+      .delete(`/api/delete/${thumbnailObjectKey}`)
+      .then(() => {
+        console.log("S3 썸네일 삭제 성공");
+        setThumbnailUrl("");
+      })
+      .catch((error) => {
+        console.log("S3 썸네일 객체 삭제 실패", error);
+      });
+  };
+
+  const handleDeleteImages = (imageUrl, productImageId) => {
+    const imageUrlObjectKey = imageUrl.split("/").pop();
+
+    axios
+      .delete(`/api/delete/${imageUrlObjectKey}`)
+      .then(() => {
+        console.log("S3 상세이미지 삭제 성공");
+
+        // 이미지 URL을 제거한 새로운 배열을 생성
+        const updatedProductImageUrls = productImageUrls.filter((url) => url !== imageUrl);
+
+        // 상태 업데이트
+        setProductImageUrls(updatedProductImageUrls);
+      })
+      .catch((error) => {
+        console.log("S3 상세이미지 삭제 실패", error);
+      });
+
+    // db에서 이미지 제거
+    axios.delete(`/shopping/img/${productImageId}`);
+  };
+
   return (
     <div>
-      <h2>상품 등록</h2>
+      <h2>상품 수정</h2>
       <form onSubmit={handleSubmit}>
         <div>
           <label>
@@ -123,7 +180,10 @@ const ProductCreate = () => {
 
         <div>
           <label>
-            썸네일:
+            썸네일: <img src={thumbnailUrl} alt="" style={{ maxWidth: "150px", maxHeight: "150px" }}></img>
+            <Button variant="outlined" color="error" onClick={() => handleDeleteThumbnail(thumbnailUrl)}>
+              삭제
+            </Button>
             <FileUploadComponent onUploadSuccess={handleUploadSuccess} />
           </label>
         </div>
@@ -147,10 +207,14 @@ const ProductCreate = () => {
           <ul>
             {productImageUrls.map((imageUrl, index) => (
               <li key={index}>
-                <img src={imageUrl} alt={`Image ${index}`} style={{ maxWidth: "100px", maxHeight: "100px" }} />
-                <button type="button" onClick={() => handleRemoveImage(index)}>
+                <img src={imageUrl} alt="" style={{ maxWidth: "100px", maxHeight: "100px" }} />
+                <Button
+                  variant="outlined"
+                  color="error"
+                  onClick={() => handleDeleteImages(imageUrl, productImageIds[index])}
+                >
                   삭제
-                </button>
+                </Button>
               </li>
             ))}
           </ul>
@@ -164,4 +228,4 @@ const ProductCreate = () => {
   );
 };
 
-export default ProductCreate;
+export default ProductUpdate;
--- src/pages/Shopping.js
+++ src/pages/Shopping.js
@@ -9,30 +9,30 @@ import Order from "../components/shop/Order";
 import { ProductDetailProvider } from "../context/ProductDetailContext";
 import CartList from "../components/shop/CartList";
 import ProductCreate from "../components/shop/ProductCreate";
-
+import ProductUpdate from "../components/shop/ProductUpdate";
 
 const Shopping = () => {
   return (
-    <ProductDetailProvider>  
-        <Routes>
-          <Route path="/shopping" element={<ProductList />} />
-          <Route path="/shopping/:pageNum" element={<ProductList />} />
-          <Route path="/shopping/search/:keyword" element={<ProductList />} />
-          <Route path="/shopping/search/:keyword/:pageNum" element={<ProductList />} />
+    <ProductDetailProvider>
+      <Routes>
+        <Route path="/shopping" element={<ProductList />} />
+        <Route path="/shopping/:pageNum" element={<ProductList />} />
+        <Route path="/shopping/search/:keyword" element={<ProductList />} />
+        <Route path="/shopping/search/:keyword/:pageNum" element={<ProductList />} />
 
-          <Route path="/shopping/category/:categoryId" element={<ProductListCategory />} />
-          <Route path="/shopping/category/:categoryId/:pageNum" element={<ProductListCategory />} />
-          <Route path="/shopping/category/:categoryId/search/:keyword" element={<ProductListCategory />} />
-          <Route path="/shopping/category/:categoryId/search/:keyword/:pageNum" element={<ProductListCategory />} />
+        <Route path="/shopping/category/:categoryId" element={<ProductListCategory />} />
+        <Route path="/shopping/category/:categoryId/:pageNum" element={<ProductListCategory />} />
+        <Route path="/shopping/category/:categoryId/search/:keyword" element={<ProductListCategory />} />
+        <Route path="/shopping/category/:categoryId/search/:keyword/:pageNum" element={<ProductListCategory />} />
 
-          <Route path="/shopping/products/:productNum" element={<ProductDetail />} />
-      
-          <Route path="/shopping/create" element={<ProductCreate />} />
+        <Route path="/shopping/products/:productNum" element={<ProductDetail />} />
 
-          <Route path="/shopping/cart/:userId" element={<CartList />} />
+        <Route path="/shopping/create" element={<ProductCreate />} />
+        <Route path="/shopping/update/:productNum" element={<ProductUpdate />} />
 
-          <Route path="/shopping/:productNum/order" element={<Order />} />
+        <Route path="/shopping/cart/:userId" element={<CartList />} />
 
+        <Route path="/shopping/:productNum/order" element={<Order />} />
       </Routes>
     </ProductDetailProvider>
   );
