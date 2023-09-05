21f72567df7fcbf829a1b01ae52069a7a8dd2d8b
Dongja <jds79793@gmail.com>
2023-09-05 17:04:16 +0900

    JIN: Feat: 커뮤니티 좋아요 기능 추가 및 인피니티 스크롤 수정

--- src/components/community/PostList.js
+++ src/components/community/PostList.js
@@ -1,19 +1,67 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
 import styles from '../../styles/community/PostList.module.css'
 import { Button } from '@material-ui/core'
 import LikeIcon from '@material-ui/icons/Favorite';
+import LikeButton from './LikeButton.js';
 
 const PostList = () => {
   const [posts, setPosts] = useState([]);
   const [pageNumb, setPageNumb] = useState(1);
   const { categoryId = 1 } = useParams();
   const loader = useRef(null);
-  const { postId } = useParams();
-  const [likeCount, setLikeCount] = useState(0);
+  const [prevCategoryId, setPrevCategoryId] = useState(categoryId);
+  const [isObserverActive, setIsObserverActive] = useState(false);
+  
+
+  // categoryId 변화 감지하여 prevCategoryId 업데이트
+  useEffect(() => {
+    setPosts([]); // 게시글 목록 초기화
+    setPageNumb(1); // 페이지 번호 초기화
+    setIsObserverActive(false);
+  }, [categoryId]);
+
+
+
+  // loader(ref) 가 화면에 나타났다 사라졌다 할 때 호출되는 함수입니다.
+  const handleObserver = useCallback((entities) => {
+    const target = entities[0];
+
+    if (target.isIntersecting) {
+      if (prevCategoryId === categoryId) {
+        setPageNumb((prevPageNumber) => prevPageNumber + 1)
+      } else {
+        setPrevCategoryId(categoryId);
+        setPageNumb((prevPageNumber) => prevPageNumber + 1)
+      }
+    }
+  }, [prevCategoryId, categoryId]); // prevCategoryId와 categoryId에 따라 함수 재생성
+
 
   useEffect(() => {
+    
+
+    // 카테고리 ID 변경 시에도 데이터 불러오기
+    axios.get(`/post/list/${categoryId}/${pageNumb}`)
+      .then(response => {
+        setPosts(prevPosts => [...prevPosts, ...response.data.content]); 
+        setIsObserverActive(true); // 데이터 로드 후 observer 활성화
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+
+  }, [categoryId, pageNumb]);
+
+
+
+
+
+
+  useEffect(() => {
+    if (!isObserverActive) return; // isObserverActive가 false일 경우 observer 생성하지 않음
+
     var options = {
       root: null,
       rootMargin: "20px",
@@ -26,58 +74,38 @@ const PostList = () => {
       observer.observe(loader.current)
     }
 
-  }, []);
+
+    // cleanup function을 추가하여 이전의 observer를 해제합니다.
+    return () => observer.disconnect();
+  }, [handleObserver, isObserverActive]); // handleObserver의 변경에 따라 새로운 observer 생성
 
 
 
-  useEffect(() => {
-    // categoryId가 변경될 때마다 pageNumb를 1로 초기화
-    setPageNumb(1);
-    setPosts([]); // 게시글 목록도 초기화합니다.
-  }, [categoryId]);
-
-  useEffect(() => {
-    // 컴포넌트가 마운트되면 HTTP 요청을 보내서 데이터를 가져옵니다.
-    axios.get(`/post/list/${categoryId}/${pageNumb}`)
-      .then(response => {
-        // 서버에서 받은 데이터를 상태에 저장
-        setPosts(prevPosts => [...prevPosts, ...response.data.content]);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-
-  }, [categoryId, pageNumb]);
 
 
 
-  const handleLike = async () => {
-    try {
-      // 서버로 좋아요 요청 보내기
-      await axios.post('/post/like', { postId, userId: 1 });
-      alert('좋아요 누르기 성공');
 
-      // 좋아요 성공 후 해당 포스트 정보 다시 가져오기
-      const response = await axios.get(`/post/${postId}`); // 좋아요 갯수만 따로 요청받을 컨트롤러 만들까 고민중
-      //좋아요 갯수만 가져오면 되는데 resource낭비
+  // const handleLike = async () => {
+  //   try {
+  //     // 서버로 좋아요 요청 보내기
+  //     await axios.post('/post/like', { postId, userId: 1 });
+  //     alert('좋아요 누르기 성공');
 
-      // 가져온 데이터를 통해 상태 갱신
-      setPosts(response.data.content);
-    } catch (error) {
-      console.error('좋아요 실패:', error);
-      alert('좋아요 실패');
-    }
-  };
+  //     // 좋아요 성공 후 해당 포스트 정보 다시 가져오기
+  //     const response = await axios.get(`/post/${postId}`); // 좋아요 갯수만 따로 요청받을 컨트롤러 만들까 고민중
+  //     //좋아요 갯수만 가져오면 되는데 resource낭비
+
+  //     // 가져온 데이터를 통해 상태 갱신
+  //     setPosts(response.data.content);
+  //   } catch (error) {
+  //     console.error('좋아요 실패:', error);
+  //     alert('좋아요 실패');
+  //   }
+  // };
 
 
 
-  // loader(ref) 가 화면에 나타났다 사라졌다 할 때 호출되는 함수입니다.
-  const handleObserver = (entities) => {
-    const target = entities[0];
-    if (target.isIntersecting) {
-      setPageNumb((prevPageNumber) => prevPageNumber + 1)
-    }
-  }
+
 
   return (
     <div className='container'>
@@ -91,18 +119,21 @@ const PostList = () => {
       <div className={styles['post-list']}>
 
         {posts.map((post) => (
-          
-            <Link to={`/community/post/${post.postId}`} className={styles['post-card']} key={post.id}  style={{ textDecoration: 'none', color: 'black' }}>
 
+
+          <div className={styles['post-card']} key={post.id}>
+            <Link to={`/community/post/${post.postId}`} style={{ textDecoration: 'none', color: 'black' }}>
               <img src={post.imageUrl1} alt={post.title} className={styles['post-image']} />
-              <LikeIcon onClick={handleLike} style={{ color: 'red', cursor: 'pointer' }} />
-              <span>{likeCount}</span>
-              <div className={styles['post-content']}>
-                <h2 className={styles['post-title']}>{post.title}</h2>
-                <p className={styles['post-author']}>{`작성자: ${post.nickname}`}</p>
-                {/* 추가적인 내용들... */}
-              </div>
             </Link>
+            <LikeButton postId={post.postId} /> {/*좋아요 버튼 component 분리, prop으로 postId 전달*/}
+            <span>{`조회수: ${post.viewCount}`}</span>
+            <div className={styles['post-content']}>
+              <h2 className={styles['post-title']}>{post.title}</h2>
+              <p className={styles['post-author']}>{`작성자: ${post.nickname}`}</p>
+              {/* 추가적인 내용들... */}
+            </div>
+
+          </div>
 
         ))}
         <div ref={loader} className={styles['loading-indicator']}>
