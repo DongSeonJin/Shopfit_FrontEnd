049ebdf5c9efe7e2918ea955acaecc446b436b8a
KwonA-Young <dkdud9817@naver.com>
2023-09-05 10:01:23 +0900

    a

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { Route, Routes } from 'react-router-dom';
 
-import UserLogin from "../components/mypage/UserLogin";
-import OrderHistory from "../components/mypage/OrderHistory";
-import OrderHistoryDetails from "../components/mypage/OrderHistoryDetails";
+import { Route, Routes } from "react-router-dom";
+// import UserLogin from "../components/mypage/UserLogin";
+import UserInfo from "../components/mypage/UserInfo";
+import UserInfoUpdate from "../components/mypage/UserInfoUpdate";
+import MyPosts from "../components/mypage/MyPosts";
 
-const Mypage = () => {
+const MyPage = () => {
     return (
         <div>
             <Routes>
-                <Route path="/login" element={<UserLogin />} />
-                <Route path="/orderhistory" element={<OrderHistory />} />
-                <Route path="/orderhistory/:orderId" element={<OrderHistoryDetails />} />
+        {/* <Route path="/login" element={<UserLogin />} /> */}
+        <Route path="/mypage/info" element={<UserInfo />} />
+        <Route path="/mypage/edit" element={<UserInfoUpdate />} />
+        <Route path="/mypage/myposts/:userId" element={<MyPosts />} />
             </Routes>
         </div>
     );
 };
+export default MyPage;
     
-export default Mypage;
