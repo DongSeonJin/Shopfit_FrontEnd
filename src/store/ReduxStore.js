cbc8611d894f79617728ba9bd6cd4d0acc7c84a3
Dongja <129161266+jds79793@users.noreply.github.com>
2023-09-18 03:14:59 +0900

    JIN: Feat: 토큰 리턱스, 쿠키에 저장 및 refreshToken Rotation 추가

--- src/store/ReduxStore.js
+++ src/store/ReduxStore.js
@@ -3,6 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "../redux/rootReducer";
 
 
+const initialState = {};
 const store = configureStore({
     reducer: rootReducer,
     devTools: true,
