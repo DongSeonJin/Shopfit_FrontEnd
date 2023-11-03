a2a10072443320b6683dde6e4295b921bf4d964a
Dongja <129161266+jds79793@users.noreply.github.com>
2023-11-03 19:54:35 +0900

    JIN: Fix: 오류난 이미지 슬라이드 삭제

--- src/components/home/HomeImageSlide.js
+++ src/components/home/HomeImageSlide.js
@@ -9,7 +9,6 @@ const HomeImageSlide = () => {
 
     const imageUrls = [
         "https://kr.object.ncloudstorage.com/post-bucket/imageslide/KakaoTalk_Photo_2023-09-12-15-03-43.png", // 샵피트 쿠폰
-        "https://file.rankingdak.com/image/RANK/BANNER/AR_IMG_1/20230907/IMG1694qCb059621087.gif", // 닭가슴살 순정마켓
         "https://file.rankingdak.com/image/RANK/BANNER/AR_IMG_1/20230830/IMG1693stS355076138.jpg", // 닭가슴살 사진
         "https://file.rankingdak.com/image/RANK/BANNER/AR_IMG_1/20230831/IMG1693WJJ464777439.jpg" // 추석 이벤트
     ];
