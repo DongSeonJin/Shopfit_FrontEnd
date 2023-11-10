4b7393838f5ee8c24ebeadf4a004c3a439b1bcbf
Dongja <129161266+jds79793@users.noreply.github.com>
2023-11-10 19:21:48 +0900

    JIN: Feat: axios 요청 주소 백엔드 서버로 변경

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/common/Header';
 import Home from './routes/Home';
 import Footer from './components/common/Footer';
+import './axiosConfig';
 
 
 import { refreshTokenApi } from './lib/api/authApi';
