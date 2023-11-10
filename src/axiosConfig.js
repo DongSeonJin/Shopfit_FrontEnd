f733a044e7d02072ce174e6b37750106b05201af
Dongja <129161266+jds79793@users.noreply.github.com>
2023-11-10 19:23:49 +0900

    JIN: Feat: axios 요청 주소 백엔드 서버로 변경

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -12,7 +12,7 @@ import axios from 'axios';
 
 
 // 로컬 개발 환경 또는 프로덕션 빌드 환경 변수 사용
-const backendUrl = 'Shopfit-env-1.eba-byhfkrys.ap-northeast-2.elasticbeanstalk.com ';
+const backendUrl = 'http://shopfit-env-1.eba-byhfkrys.ap-northeast-2.elasticbeanstalk.com/';
 
 axios.interceptors.request.use(config => {
   
