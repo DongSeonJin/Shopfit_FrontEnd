4ce7b0275f126f82f5d723e7e8d7f4919b74e472
Dongja <129161266+jds79793@users.noreply.github.com>
2023-11-11 23:59:00 +0900

    JIN: Refactor: readme.md 수정

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -12,11 +12,12 @@ import axios from 'axios';
 
 
 // 로컬 개발 환경 또는 프로덕션 빌드 환경 변수 사용
-const backendUrl = 'http://shopfit-env-1.eba-byhfkrys.ap-northeast-2.elasticbeanstalk.com';
+const backendUrl = 'http://shopfit-env-1.eba-byhfkrys.ap-northeast-2.elasticbeanstalk.com/';
 
 axios.interceptors.request.use(config => {
-  
+  if (!config.url.startsWith('/')) {
+    // 외부 서비스나 프로덕션 백엔드 URL을 요청하는 경우
     config.url = backendUrl + config.url;
-  
+  }
   return config;
 });
